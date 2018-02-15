0d432042cb690b54bff7455ff889331f46d8398b
Arda Ozcan <ardaoezcan@gmail.com>
2018-02-15 16:36:33 +0100

    building views

---
+++ db/migrate/20180215110228_add_description_to_doses.rb
@@ -0,0 +1,7 @@
+class AddDescriptionToDoses < ActiveRecord::Migration[5.1]
+  def change
+    add_column :doses, :description, :string
+    add_reference :doses, :ingredient, foreign_key: true
+    add_reference :doses, :cocktail, foreign_key: true
+  end
+end
